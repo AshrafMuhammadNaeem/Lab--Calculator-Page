1ef4fd52565a73506670cb4ae09e04fbe8d94200
Naeem Ashraf <naeemashraf.tud@gmail.com>
2020-10-02 15:46:00 +0500

    inner html modification

--- Labcal.js
+++ Labcal.js
@@ -1,1 +1,4 @@
-window.prompt("Please enter your name:");
+window.prompt("Please enter your name");
+var username = window.prompt("Please enter your name");
+var greetingParagraph = document.getElementByID.("greetings");
+greetingParagraph.innerHTML += "," + username ;
