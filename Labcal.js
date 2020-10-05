9eb1e482b8ad72df431b64b02844731455160ddd
Naeem Ashraf <naeemashraf.tud@gmail.com>
2020-10-05 09:47:13 +0500

    prompt & store of number variable

--- Labcal.js
+++ Labcal.js
@@ -1,3 +1,10 @@
 var userName = window.prompt("Please enter your name");
 var greetingParagraph = document.getElementById("greetings");
 greetingParagraph.innerHTML += "," + userName ;
+var number1 = window.prompt("Please enter number1");
+var operand1 = document.getElementById("operand1");
+operand1.innerHTML += number1 ;
+var number2 = window.prompt("Please enter number1");
+var operand2 = document.getElementById("operand2");
+operand2.innerHTML += number2;
+
